f054f1307336fb1397bdfd32027137a79e417c9e
SeanMcSweeney <ssmcsweeney@gmail.com>
2020-04-22 18:19:47 +0100

    added fix for break in loop

--- GamesEnginesExam/Assets/Scripts/TrafficState.cs
+++ GamesEnginesExam/Assets/Scripts/TrafficState.cs
@@ -37,75 +37,80 @@ public class TrafficState : MonoBehaviour
 
     private IEnumerator ChangeColour(GameObject light)
     {
+        Debug.Log("Started ChangeColour");
+        usingState = false;
         while (true)
         {
-            Debug.Log("Started ChangeColour");
-            usingState = false;
-            while (true){
-                if (usingState == false){
-                    int colour = Random.Range(1,3);
-                    
-                    // 1 = green, 2 = yellow, 3 = red.
+            if (usingState == false){
+                int colour = Random.Range(1,3);
+                
+                // 1 = green, 2 = yellow, 3 = red.
 
-                    if (colour == 1){
-                        green = true;
-                        yellow = false;
-                        red = false;
-                    }
-                    else if (colour == 2){
-                        green = false;
-                        yellow = true;
-                        red = false;
-                    }
-                    else if (colour == 3){
-                        green = false;
-                        yellow = false;
-                        red = true;
-                    }
-                    else{
-                        Debug.Log("error colour not chosen");
-                        yield return null;
-                    }
-
-                    // set colours and times
-
-                    if (green == true){
-                        int time = Random.Range(5,10);
-                        float finaltime = time;
-                        timer += Time.deltaTime;
-                        while(timer <= finaltime){
-                            usingState = true;
-                            var lightRend = light.GetComponent<Renderer>();
-                            lightRend.material.SetColor("_Color", Color.green);
-                        }
-                    }
-
-                    if (yellow == true){
-                        int time = 4;
-                        float finaltime = time;
-                        timer += Time.deltaTime;
-                        while(timer <= finaltime){
-                            usingState = true;
-                            var lightRend = light.GetComponent<Renderer>();
-                            lightRend.material.SetColor("_Color", Color.yellow);
-                        }
-                    }
-
-                    if (red == true){
-                        int time = Random.Range(5,10);
-                        float finaltime = time;
-                        timer += Time.deltaTime; 
-                        while(timer <= finaltime){
-                            usingState = true;
-                            var lightRend = light.GetComponent<Renderer>();
-                            lightRend.material.SetColor("_Color", Color.green);
-                        }                   
-                    }
-
+                if (colour == 1){
+                    green = true;
+                    yellow = false;
+                    red = false;
+                }
+                else if (colour == 2){
+                    green = false;
+                    yellow = true;
+                    red = false;
+                }
+                else if (colour == 3){
+                    green = false;
+                    yellow = false;
+                    red = true;
                 }
                 else{
+                    Debug.Log("error colour not chosen");
+                    yield return null;
                 }
+
+                // set colours and times
+
+                if (green == true){
+                    int time = Random.Range(5,10);
+                    float finaltime = time;
+                    timer += Time.deltaTime;
+                    while(timer <= finaltime){
+                        usingState = true;
+                        var lightRend = light.GetComponent<Renderer>();
+                        lightRend.material.SetColor("_Color", Color.green);
+                        yield return new WaitForFixedUpdate();
+                    }
+                    usingState = false;
+                }
+
+                if (yellow == true){
+                    int time = 4;
+                    float finaltime = time;
+                    timer += Time.deltaTime;
+                    while(timer <= finaltime){
+                        usingState = true;
+                        var lightRend = light.GetComponent<Renderer>();
+                        lightRend.material.SetColor("_Color", Color.yellow);
+                        yield return new WaitForFixedUpdate();
+                    }
+                    usingState = false;
+                }
+
+                if (red == true){
+                    int time = Random.Range(5,10);
+                    float finaltime = time;
+                    timer += Time.deltaTime; 
+                    while(timer <= finaltime){
+                        usingState = true;
+                        var lightRend = light.GetComponent<Renderer>();
+                        lightRend.material.SetColor("_Color", Color.green);
+                        yield return new WaitForFixedUpdate();
+                    }             
+                    usingState = false;
+                }
+
             }
+            else{
+            }
+            yield return new WaitForFixedUpdate();
         }
     }
 }
