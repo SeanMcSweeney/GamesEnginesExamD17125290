b67adbd522cd7b860199d1fa3ab83243508948b7
SeanMcSweeney <ssmcsweeney@gmail.com>
2020-04-22 18:07:54 +0100

    Couroutines Fix

---
+++ GamesEnginesExam/Assets/Scripts/TrafficState.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrafficState : MonoBehaviour
+{
+    float timer;
+    bool usingState;
+    bool green;
+    bool yellow;
+    bool red;
+
+    float amountoflights;
+    trafficLights tl;
+    void Start()
+    {
+        //tl= GetComponent<trafficLights>();
+        //amountoflights = tl.amountoflights;
+        //ChooseColour(amountoflights);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChooseColour(float amountoflights)
+    {
+        for (float i = 0; i < amountoflights; i++){
+        Debug.Log("Started " + i);
+        GameObject light = GameObject.Find("Traffic Light " + (i + 1));
+        StartCoroutine(ChangeColour(light));
+        }
+        Debug.Log(amountoflights);
+    }
+
+    private IEnumerator ChangeColour(GameObject light)
+    {
+        while (true)
+        {
+            Debug.Log("Started ChangeColour");
+            usingState = false;
+            while (true){
+                if (usingState == false){
+                    int colour = Random.Range(1,3);
+                    
+                    // 1 = green, 2 = yellow, 3 = red.
+
+                    if (colour == 1){
+                        green = true;
+                        yellow = false;
+                        red = false;
+                    }
+                    else if (colour == 2){
+                        green = false;
+                        yellow = true;
+                        red = false;
+                    }
+                    else if (colour == 3){
+                        green = false;
+                        yellow = false;
+                        red = true;
+                    }
+                    else{
+                        Debug.Log("error colour not chosen");
+                        yield return null;
+                    }
+
+                    // set colours and times
+
+                    if (green == true){
+                        int time = Random.Range(5,10);
+                        float finaltime = time;
+                        timer += Time.deltaTime;
+                        while(timer <= finaltime){
+                            usingState = true;
+                            var lightRend = light.GetComponent<Renderer>();
+                            lightRend.material.SetColor("_Color", Color.green);
+                        }
+                    }
+
+                    if (yellow == true){
+                        int time = 4;
+                        float finaltime = time;
+                        timer += Time.deltaTime;
+                        while(timer <= finaltime){
+                            usingState = true;
+                            var lightRend = light.GetComponent<Renderer>();
+                            lightRend.material.SetColor("_Color", Color.yellow);
+                        }
+                    }
+
+                    if (red == true){
+                        int time = Random.Range(5,10);
+                        float finaltime = time;
+                        timer += Time.deltaTime; 
+                        while(timer <= finaltime){
+                            usingState = true;
+                            var lightRend = light.GetComponent<Renderer>();
+                            lightRend.material.SetColor("_Color", Color.green);
+                        }                   
+                    }
+
+                }
+                else{
+                }
+            }
+        }
+    }
+}
+
--- GamesEnginesExam/Assets/Scripts/trafficLights.cs
+++ GamesEnginesExam/Assets/Scripts/trafficLights.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class trafficLights : MonoBehaviour
 {
+    public float amountoflights;
+    TrafficState ts;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,20 +18,31 @@ public class trafficLights : MonoBehaviour
         
     }
 
-    void CreateInCircle(){
+    void StateManager(){
+        
+    }
+
+
+    public void CreateInCircle(){
         float circleSize = 8f;
-        float amountoflights = 10f;
+        amountoflights = 10f;
         float height = 2f;
 
         for (int i = 0; i < amountoflights; i++)
         {
             GameObject capsule = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+            GameObject capsuleSetPos = Instantiate(capsule, new Vector3(0, 0, 0), Quaternion.identity);
             float angle = i * Mathf.PI * 2f / amountoflights;
             Vector3 CirclePos = new Vector3(Mathf.Cos(angle) * circleSize, height, Mathf.Sin(angle) * circleSize);
-            GameObject gameObj = Instantiate(capsule, CirclePos, Quaternion.identity);
+            GameObject gameObj = Instantiate(capsuleSetPos, CirclePos, Quaternion.identity);
+            gameObj.name = "Traffic Light " + (i + 1);
 
             // Delete the created game objects without removing the circle of lights
             Destroy(capsule);
+            Destroy(capsuleSetPos);
         }
+
+        ts = GetComponent<TrafficState>();
+        ts.ChooseColour(amountoflights);
     }
 }
